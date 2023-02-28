b19f7be913b964e03a2e6a09dd9e245929352c22
Aubrey Guishard <aubrey.guishard@gmail.com>
2023-02-28 10:29:48 -0500

    Created axios function to retrieve all of the video games + created state variables for video games

--- video-game-react/frontend/src/App.jsx
+++ video-game-react/frontend/src/App.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import SearchBar from './Components/SearchBar/SearchBar';
 import VideoGameTracker from './Components/VideoGameTracker/VideoGameTracker';
 function App() {
+
+  const [videoGames, setVideoGames] = useState([]);
+
+  useEffect(() => {
+    getVideogames();
+  }, []);
+
+  async function getVideogames(){
+    try{
+      debugger;
+      const response = await axios.get('http://localhost:8080/all');
+      setVideoGames(response.data);
+    } catch(exception){
+      console.log(`EROR in getVideoGames EXCEPTION: ${exception}`)
+    }
+
+  }
   return (
     <div >
 
