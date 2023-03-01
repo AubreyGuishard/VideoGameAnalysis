dcb8efe02a158109a9d2bdcdb8d31a20b0e1e83a
natalievmedrano <nataliemed0403@gmail.com>
2023-03-01 14:42:41 -0600

    created pie chart to display sales by region whenever game is searched for

--- video-game-react/frontend/src/Components/StatsTracker/StatsTracker.jsx
+++ video-game-react/frontend/src/Components/StatsTracker/StatsTracker.jsx
@@ -1,49 +1,76 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { Chart } from "react-google-charts";
-
 
 //WHAT GAMES SELL THE MOST GLOBALLY
 
 // WHAT DO WE WANT TO DO: we want to get the sales for each country and then display them on the pie chart
 
-
-
 // our question : we are having trouble trying to access specific regional sales for our chart
 
-// we know we have to filter through the sales and then probably map through them we are having trouble connecting that logic 
-const StatsTracker= ({filteredVideoGames}) => {
+const StatsTracker = ({ filteredVideoGames }) => {
 
-function generateSalesForm (){
-    let northAmericaSales= filteredVideoGames.filter(game => game.northamericasales)
-    console.log('NA SALES', northAmericaSales)
 
-  
 
-    
-}
+
+
+
+
+
+
+
+  function generateSalesForm() {
+    let northAmericaSales = 0;
+    let europeSales = 0;
+    let japanSales = 0;
+    let otherSales = 0;
+
+
+
+    filteredVideoGames.forEach((game) => {
+      northAmericaSales += game.northamericasales;
+      europeSales += game.europesales;
+      japanSales += game.japansales;
+      otherSales += game.othersales;
+    });
+
 
     const data = [
         ["Task", "Hours per Day"],
-        ["North America Sales", 11],
-        ["European Sales", 2],
-        ["Japanese Sales", 2],
-        ["Other Sales", 2],
+        ["North America Sales", northAmericaSales],
+        ["European Sales", europeSales],
+        ["Japanese Sales", japanSales],
+        ["Other Sales", otherSales],
       ];
-      
-       const options = {
-        title: "GLOBAL SALES FOR EACH COUNTRY",
-        is3D: true,
-      };
-      return( 
-        <Chart
+      return data;
+  }
+
+
+
+
+
+
+
+
+
+  useEffect(()=> {
+    generateSalesForm()
+  },[filteredVideoGames])
+
+  
+
+  const options = {
+    title: "SALES FOR EACH COUNTRY",
+    is3D: true,
+  };
+  return (
+    <Chart
       chartType="PieChart"
       data={generateSalesForm()}
       options={options}
       width={"100%"}
       height={"400px"}
     />
-      );
-      
-}
- 
-export default StatsTracker ;
+  );
+};
+
+export default StatsTracker;
